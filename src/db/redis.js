8d5dc55010e7c8e003acde76b843b4d1d416129b
zxycat2 <zxycat2@sina.cn>
2019-09-22 03:53:22 +0100

    redis

---
+++ src/db/redis.js
@@ -0,0 +1,46 @@
+const {REDIS_CONFIG} = require('../config/db');
+const redis = require('redis');
+
+const redisCli =  redis.createClient(REDIS_CONFIG.port, REDIS_CONFIG.host);
+redisCli.on('error', (err) => {
+    console.error(err);
+})
+
+//get func
+function setDataToRedis(key, val){
+    if (typeof val == Object){
+        val = JSON.stringify(val);
+    }
+    redisCli.set(key, val, redis.print);
+}
+
+//set func
+function getDataFromRedis(key){
+    let promise = new Promise( (resolve, reject) => {
+        redisCli.get(key, (err, val) => {
+            if (err){
+                reject(err);
+                return;
+            }
+
+            if (val == null){
+                resolve(null);
+                return;
+            }
+            //尝试把val转换回obj
+            try{
+                resolve(JSON.parse(val));
+            }catch(ex){
+                resolve(val);
+            }
+            return;
+        })
+    })
+    return promise;
+}
+
+module.exports = {
+    setDataToRedis,
+    getDataFromRedis
+};
+
