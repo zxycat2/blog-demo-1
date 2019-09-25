1f32eaa0614e3efbaf008beed7339f0c84fe31cc
zxycat2 <zxycat2@sina.cn>
2019-09-25 18:55:16 +0100

    nginx?

--- src/router/user.js
+++ src/router/user.js
@@ -1,5 +1,6 @@
 const {SuccessModel, ErrorModel} = require('../models/resModel')
 const {login} = require('../controller/user');
+const {setDataToRedis} = require('../db/redis');
 
 //生成cookie过期时间
 const getCookieExpiresData = () => {
@@ -10,15 +11,15 @@ const getCookieExpiresData = () => {
 
 const handleUserRouter = (req, res) => {
     //登录请求
-    if (req.method === 'GET' && req.path == '/api/user/login'){
-        const {username, password} = req.query;
+    if (req.method === 'POST' && req.path == '/api/user/login'){
+        const {username, password} = req.body;
         return login(username, password).then( result => {
             if (result.realname){
                 //设置session
                 req.session.username = result.username;
                 req.session.realname = result.realname;
-
-                console.log('req session agter log in', req.session);
+                //同步到redis
+                setDataToRedis(req.sessionID, req.session);
                 return new SuccessModel('登录成功');
             }else{
                 return new ErrorModel('登录失败');
@@ -27,14 +28,14 @@ const handleUserRouter = (req, res) => {
     }
 
     //登录测试
-    if (req.method === 'GET' && req.path == '/api/user/login-test'){
-        console.log(req.session);
-        if (req.session.username){
-            return Promise.resolve(new SuccessModel({session:req.session}, '登录测试成功'));
-        }else{
-            return Promise.resolve(new ErrorModel('登录测试失败'))
-        }
-    }
+    // if (req.method === 'GET' && req.path == '/api/user/login-test'){
+    //     console.log('req.session in login-test', req.session);
+    //     if (req.session.username){
+    //         return Promise.resolve(new SuccessModel({session:req.session}, '登录测试成功'));
+    //     }else{
+    //         return Promise.resolve(new ErrorModel('登录测试失败'))
+    //     }
+    // }
 }
 
 module.exports = {handleUserRouter, getCookieExpiresData};
